e68cb3ee94d0fa8b17afc5f1647e3e50177cfa95
mohammad <mobehfard@gmail.com>
2026-08-17 11:28:45 +0330

    create new expends def

---
+++ core/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import random
+
+
+app = FastAPI()
+
+manage_list = []
+
+
+@app.post("/expends")
+def Create_New_Expense(description: str ,amount: float):
+    while True:
+        id = random.randint(1,100)
+        uniq = True
+        for item in manage_list:
+            if item["id"] == id:
+                uniq = False
+        if uniq == True:
+            break
+
+    item = {"id" : id, "description" : description, "amount" : amount}
+    manage_list.append(item)
+    return item
+                
+
